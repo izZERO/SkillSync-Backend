83f37a2921523351fe867e159d6fdf7a3a3ad6c6
unknown <rabab177hasan@gmail.com>
2025-10-24 21:45:33 +0300

    getting all lessons controller

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -1,2 +1,12 @@
 const Lesson = require("../models/Lesson.js")
 
+const get_lessons = async (req, res) => {
+  try {
+    const lessons = await Lesson.find({ course: req.params.courseId}).sort("order")
+    res.status(200).send(lessons)
+  }
+  catch (error) {
+    res.status(500).send({msg: "Error getting the lessons", error})
+  }
+
+}
