6c941425fa99a310a92706f30f60ad2dcd7e41fb
unknown <rabab177hasan@gmail.com>
2025-10-25 16:40:40 +0300

    create and update done

--- controllers/lessons.js
+++ controllers/lessons.js
@@ -1,7 +1,7 @@
 const Lesson = require("../models/Lesson.js")
 const Course = require("../models/Course.js")
 
-const get_lessons = async (req, res) => {
+const lessons_index_get = async (req, res) => {
   try {
     const lessons = await Lesson.find({ course: req.params.courseId})
     res.status(200).send(lessons)
@@ -11,14 +11,59 @@ const get_lessons = async (req, res) => {
   }
 }
 
-const get_lesson = async (req, res) => {
+const lesson_show_get = async (req, res) => {
   try {
     const lesson = await Lesson.findById(req.params.lessonId)
     if (!lesson) return res.status(404).send({msg:"lesson not found"})
-res.status(200).send(lessons)
+res.status(200).send(lesson)
   }
   catch (error) {
     res.status(500).send({msg: "Error getting the lesson", error})
+  }
+}
+
+const lesson_new_post =async (req, res) => {
+  try{
+    const instructorId = res.locals.payload.id
+    const course = await Course.findById(req.body.course)
+    if(!course) {
+      return res.status(404).send({msg: "course not found"})
+    }
+    if (course.instructor.toString() !== instructorId){
+return res.status(404).send({msg: "unautharized"})
+    }
+    const existingLessons = await Lesson.find({course:req.body.course})
+    const order = existingLessons.length+1
+    const lesson = await Lesson.create({
+      ...req.body,
+      order,
+    })
+    course.lessons.push(lesson._id)
+    await course.save()
+
+    res.status(200).send(lesson)
+  }
+  catch (error) {
+  res.status(500).send({ msg: "Error creating a new lesson", error })
+}
+
+}
+const lesson_update_put = async (req, res) => {
+  try {
+    const currentUser = res.locals.payload.id
+    const lesson = await Course.findById(req.params.lessonId)
+    if (!lesson) {
+      res.status(404).send({ msg: "lesson not found!", error })
+    }
+
+    if (course.instructor.toString() !== currentUser) {
+      res.status(403).send({ status: "Error", msg: "Unauthorized" })
+    } else {
+      await Lesson.updateOne({ _id: lesson._id }, req.body)
+      res.status(200).send("Lesson Updated!")
+    }
+  } catch (error) {
+    res.status(500).send({ msg: "Error editing a lesson!", error })
   }
 }
 
@@ -36,4 +81,4 @@ res.status(200).send(lessons)
 
 
 
-module.exports ={ get_lessons,get_lesson}
+module.exports ={ lessons_index_get,lesson_show_get,lesson_new_post,lesson_update_put}
--- routes/lessons.js
+++ routes/lessons.js
@@ -1,11 +1,11 @@
 const router = require("express").Router()
 const lessonCtrl = require("../controllers/lessons")
 
-router.get('/:courseId', lessonCtrl.get_lessons)
+router.get('/:courseId', lessonCtrl.lessons_index_get)
+router.get("/:lessonId", lessonCtrl.lesson_show_get)
+router.post("/",lessonCtrl.lesson_new_post)
 
-
-
-
+router.put("/:lessonId", lessonCtrl.lesson_update_put)
 
 
 module.exports = router
